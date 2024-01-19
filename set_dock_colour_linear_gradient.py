1690a69a5296f38c3807a4063f35e909504847df
NPastorale <npastorale@gmail.com>
2024-01-19 10:32:42 +0100

    cleanup

--- set_dock_colour_linear_gradient.py
+++ set_dock_colour_linear_gradient.py
@@ -22,8 +22,6 @@ device_manager.sync_effects = False
 
 
 def get_dock():
-    # device_manager = razer.DeviceManager()
-    # device_manager.sync_effects = False
     try:
         for device in device_manager.devices:
             if "Razer Mouse Dock" == device.name:
@@ -34,8 +32,6 @@ def get_dock():
 
 
 def get_mouse():
-    # device_manager = razer.DeviceManager()
-    # device_manager.sync_effects = False
     try:
         for device in device_manager.devices:
             if "Razer Viper Ultimate (Wireless)" == device.name:
@@ -48,15 +44,15 @@ def get_mouse():
 def set_dock_colour(mouse_battery, dock):
     if mouse_battery >= UPPER_THRESHOLD:
         dock.fx.static(0, 255, 0)
-        print("Batttery is at {}%, setting dock to r:0 g:255 b:0".format(mouse_battery))
+        print("Battery is at {}%, setting dock to r:0 g:255 b:0".format(mouse_battery))
     elif mouse_battery > LOWER_THRESHOLD:
         dock.fx.static(gradient['r'][mouse_battery-LOWER_THRESHOLD],
                        gradient['g'][mouse_battery-LOWER_THRESHOLD], gradient['b'][mouse_battery-LOWER_THRESHOLD])
-        print("Batttery is at {}%, setting dock to r:{} g:{} b:{}".format(
+        print("Battery is at {}%, setting dock to r:{} g:{} b:{}".format(
             mouse_battery, gradient['r'][mouse_battery-LOWER_THRESHOLD], gradient['g'][mouse_battery-LOWER_THRESHOLD], gradient['b'][mouse_battery-LOWER_THRESHOLD]))
     else:
         dock.fx.static(255, 0, 0)
-        print("Batttery is at {}%, setting dock to r:255 g:0 b:0".format(mouse_battery))
+        print("Battery is at {}%, setting dock to r:255 g:0 b:0".format(mouse_battery))
 
 
 def set_brightness(mouse):
@@ -69,13 +65,8 @@ def set_brightness(mouse):
 while True:
     mouse_battery_previous = mouse_battery_current
     mouse_battery_current = get_mouse().battery_level
-    # mouse_charging_state_previous = mouse_charging_state_current
-    # mouse_charging_state_current = get_mouse().is_charging
 
     if mouse_battery_current != mouse_battery_previous:
         set_dock_colour(mouse_battery_current, get_dock())
-
-    # if mouse_charging_state_current != mouse_charging_state_previous:
-    #     set_brightness(get_mouse())
 
     time.sleep(1)
